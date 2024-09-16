3cc72fa01a354cc7e9450fc58a21adcc7995d0e1
davis-matthew <mdavis438@gatech.edu>
2024-09-16 02:13:14 -0400

    Module Generation

---
+++ THE-LLVMTranslator/THE-Instruction.h
@@ -0,0 +1,209 @@
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+
+#include <string>
+#include <vector>
+
+/*
+The reason we have a "THEInstruction" class instead of just an instruction is to 
+differentiate and disambiguate with the LLVM::Instruction class that we will be
+using in the generation of LLVM IR
+*/
+std::vector<string> split(const string& s, char splitChar) {
+    vector<string> temp;
+    stringstream strs(s);
+    string t = "";
+    while (getline(strs, t, splitChar)) { temp.push_back(t); }
+    return temp;
+}
+
+enum THEInstructionType {
+    Add, Subtract, Concat, Mult, Divide, Modulo, Power, And, Or, Not,
+	BitAnd, BitOr, BitNot,
+	Less, Greater, LessEqual, GreaterEqual, Equal, NotEqual, RefEqual, RefNotEqual,
+	Call,
+	Print, // Print to the "best" console? stdout?
+	Read, // Read a from memory (base type or array)
+	WriteToReference, // Write to a reference to a variable or memory
+	GetReference, // Get a pointer to a variable/array/struct
+	ReadBuiltInProperty, // Read a property of an object, such as length of an array or other special property.
+	Given, // Load a value from program memory (like a constant)
+	Reassign, // Change the value of an existing variable (or index in an array)
+	Alloc, // Allocate memory, but without assigning it to any variable
+	Initialize, // Initialize a new variable and assign it to something
+	Declare, // Declare the scope of a variable without assigning it to anything???
+	ArrayLength, // Read the length of an array
+	Break, // Instantly jump out of the end of the nearest loop
+	Continue, // Jump directly to the nearest loop header
+	If, ElseIf, Else, EndBlock, StartBlock, Loop, FunctionDefinition
+};
+
+const std::string ALPHABET_CASEINSENSITIVE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+class THEInstruction {
+    THEInstructionType type;
+    std::vector<THEInstruction> args;
+
+
+    llvm::Value* res = nullptr;
+public:
+    THEInstruction(std::string t) {
+        parseInstruction(t);
+        t = trimLinePrefix(t);
+        commandType = getCommand(t);
+        //Command 'string'? (arg1, arg2, ...)?->?type of var 'name' Parent=#
+
+    }
+
+    void parseInstruction(std::string t) {
+        //Trim prefix
+        size_t pos = line.find_first_of(ALPHABET_CASEINSENSITIVE);
+        if (pos != std::string::npos) {
+            t = line.substr(pos);
+        }
+        else{
+            t = "";
+        }
+
+        //Command
+        pos = line.find_first_not_of(ALPHABET_CASEINSENSITIVE);
+        std::string command = line.substr(0,pos);
+        line = line.substr(pos);
+        
+        if (line[0] == '(') {
+            size_t end = line.find(')');
+            args = split(line.substr(1, end - 2), ',');
+            line = line.substr(end+1);
+        }
+
+        if (line[0] == ' ' && line[1] == '\'') {
+            //parse Name
+        }
+
+        if (line[0] == '-' && line[1] == '>') {
+            //parse RHS
+        }
+
+        //parse type
+        //parse name
+        //parse parent
+    }
+    
+    void createInstructionInIR(llvm::Module &M, llvm::IRBuilder<> &builder) {
+        if (res != nullptr) { return; } // Skip if we've already created this instruction
+
+        std::vector<llvm::Value*> IRargs = std::vector<llvm::Value*>(args.size());
+        for(int i = 0; i < args.size(); i++){
+            IRargs[i] = args[i].getResultIRInst();
+        }
+
+        switch(type){
+
+            // Operations
+            case(Add): { 
+                res = builder.CreateAdd(IRargs[0], IRargs[1]); 
+                break;
+            }
+            case(Subtract): {
+                res = builder.CreateSub(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(Concat):{}
+            case(Mult): {
+                res = builder.CreateMul(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(Divide):{
+                res = builder.CreateSDiv(IRargs[0], IRargs[1]); // signed division
+                break;
+            }
+            case(Modulo):{
+                res = builder.CreateSRem(IRargs[0], IRargs[1]); // signed remainder
+                break;
+            }
+            case(Power):{}
+            case(And): {
+                res = builder.CreateAnd(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(Or): {
+                res = builder.CreateOr(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(Not): {
+                res = builder.CreateNot(IRargs[0]);
+                break;
+            }
+            case(BitAnd):{}
+            case(BitOr):{}
+            case(BitNot):{}
+
+            // Comparisons
+            case(Less):{
+                res = builder.CreateICmpSLT(IRargs[0], IRargs[1]); // signed comparison
+                break;
+            }
+            case(Greater):{
+                res = builder.CreateICmpSGT(IRargs[0], IRargs[1]); // signed comparison
+                break;
+            }
+            case(LessEqual):{
+                res = builder.CreateICmpSLE(IRargs[0], IRargs[1]); // signed comparison
+                break;
+            }
+            case(GreaterEqual):{
+                res = builder.CreateICmpSGE(IRargs[0], IRargs[1]); // signed comparison
+                break;
+            }
+            case(Equal):{
+                res = builder.CreateICmpEQ(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(NotEqual):{
+                res = builder.CreateICmpNE(IRargs[0], IRargs[1]);
+                break;
+            }
+            case(RefEqual):{}
+            case(RefNotEqual):{}
+
+            // Various Other
+            case(Call):{}
+            case(Print):{}
+            case(Read):{}
+            case(WriteToReference):{}
+            case(GetReference):{}
+            case(ReadBuiltInProperty):{}
+            case(Given):{}
+            case(Reassign):{}
+            case(Alloc):{}
+            case(Initialize):{}
+            case(Declare):{}
+            case(ArrayLength):{}
+
+            // Control Flow
+            case(Break):{}
+            case(Continue):{}
+            case(If):{}
+            case(ElseIf):{}
+            case(Else):{}
+            case(EndBlock):{}
+            case(StartBlock):{}
+            case(Loop):{}
+            case(FunctionDefinition):{}
+            default: {
+                throw std::runtime_error("Unknown instruction type.");
+            }
+        }
+    }
+
+    llvm::Value* getResultIRInst(){
+        if (res == nullptr) {
+            //res = createInstructionInIR();
+        }
+        return res;
+    }
+
+    void setResultIRInst(llvm::Value &I){
+        res = &I;
+    }
+};
--- THE-LLVMTranslator/THE-LLVMTranslator.cpp
+++ THE-LLVMTranslator/THE-LLVMTranslator.cpp
@@ -1,51 +1,134 @@
-#include "llvm/IR/Module.h"
-#include "llvm/IR/LLVMContext.h"
+#include <iostream>
+#include <fstream>
+#include <optional>
+
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/LegacyPassManager.h>
+
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <llvm/Target/CodeGenCWrappers.h>
+#include <llvm/Target/TargetMachine.h>
+
+#include <llvm/MC/TargetRegistry.h>
+
+#include <llvm/TargetParser/Host.h>
+#include <llvm/TargetParser/SubtargetFeature.h>
+
+#include <llvm/Object/ObjectFile.h>
+
+#include <llvm/Bitcode/BitcodeWriter.h>
+
+#include "THE-Instruction.h"
 
 using namespace llvm;
 
+std::string filename = "";
+llvm::LLVMContext context;
+std::vector<THEInstruction> insts;
+Module *M = nullptr;
+TargetMachine *targetMachine;
+
 //https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl08.html
 
-Module* convertFileToLLVMIR(){
-    LLVMContext context;
-    Module* module = new Module("converted", context);
+void processArgs(int argc, char** argv) {
+    if(argc == 1){
+        std::cout << "Usage: THE-LLVMTranslator <path/to/THE-COMPILER/file>\n";
+        std::terminate();
+    }
+    filename = argv[1];
+}
+void loadFile() {
+    insts = std::vector<THEInstruction>();
+    std::ifstream file(filename);
+    std::string line;
 
+    if (file.is_open()) {
+        while (std::getline(file, line)) {
+            insts.push_back(THEInstruction(line));
+        }
+        file.close();
+    } 
+    else {
+        std::cerr << "Cannot open file!" << std::endl;
+    }
+}
+void generateModule() {
+//     InitializeAllTargetInfos();
+//     InitializeAllTargets();
+//     InitializeAllTargetMCs();
+//     InitializeAllAsmParsers();
+//     InitializeAllAsmPrinters();
 
-    FunctionType *foo_type = TypeBuilder<int(int, char **), false>::get(getGlobalContext());
-    Function *func = cast<Function>(mod->getOrInsertFunction("main", foo_type));
-    BasicBlock *block = BasicBlock::Create(getGlobalContext(), "entry", func);
-    IRBuilder<> builder(block);
-    // (15 * 10) + 5 = ?
-    Value *tmp = builder.CreateMul(builder.getInt32(15), builder.getInt32(10), "mul");
-    Value *tmp2 = builder.CreateAdd(tmp, builder.getInt32(5), "add");
-    builder.CreateRet(tmp2);
+    
+    M = new Module("filename", context); //TODO: filename
+    IRBuilder<> builder(context);
 
-    return module;
+// std::cout << "a1\n";
+    
+//     auto TargetTriple = sys::getDefaultTargetTriple();
+//     M->setTargetTriple(TargetTriple);
+//     std::cout << TargetTriple << "\n";
+//     std::string Error;
+//     auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+//     std::cout <<"ERROR: " << Error << "\n";
+// std::cout << "a2\n";
+//     auto RM = std::optional<Reloc::Model>();
+//     TargetOptions opt;
+//     targetMachine = Target->createTargetMachine(TargetTriple, "generic", "", opt, RM);
+// std::cout << "a3\n";
+//     auto DataLayout = targetMachine->createDataLayout();
+//     M->setDataLayout(DataLayout);
+
+    
+
+    auto i32 = builder.getInt32Ty();
+    auto mainType = FunctionType::get(i32, false);
+    auto mainFunc = Function::Create(mainType, Function::ExternalLinkage, "main", M);
+    auto entry = BasicBlock::Create(context, "entry", mainFunc);
+    builder.SetInsertPoint(entry);
+
+    auto printfType = FunctionType::get(builder.getInt32Ty(), PointerType::getUnqual(builder.getInt8Ty()), true);
+    auto printfFunc = Function::Create(printfType, Function::ExternalLinkage, "printf", M);
+    auto helloWorld = builder.CreateGlobalStringPtr("Hello World!\n");
+    builder.CreateCall(printfFunc, {helloWorld});
+    builder.CreateRet(builder.getInt32(0));
+
+    std::error_code EC;
+    raw_fd_ostream OS("filename.bc", EC, sys::fs::OF_None);
+    WriteBitcodeToFile(*M, OS);
+
 }
 
-void runLLVMPasses(Module &M){
+void runLLVMPasses() {
     //new pass manager
     //reference opt?
 }
 
-void generateExecutable(Module &M){
-    //https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl08.html
+void generateExecutable(){
+    // raw_fd_ostream dest("a.out", nullptr, sys::fs::OF_None);
 
-    auto TargetTriple = sys::getDefaultTargetTriple();
-    std::string Error;
-    auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+    // legacy::PassManager PM;
+    // targetMachine->addPassesToEmitFile(PM,dest, nullptr, CodeGenFileType::ObjectFile);
 
-    auto CPU = "generic";
-    auto Features = "";
-
-    TargetOptions opt;
-    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, Reloc::PIC_);
-
-    M->setDataLayout(TargetMachine->createDataLayout());
-    M->setTargetTriple(TargetTriple);
+    // PM.run(*M);
+    // dest.flush();
 }
 
-int main(int argc, char **argv) { 
-    Module* m = convertFileToLLVMIR();
-    runLLVMPasses(m);
-    generateExecutable(m);
+int main(int argc, char **argv) {
+    //std::cout << "1\n";
+    processArgs(argc, argv);
+    //std::cout << "2\n";
+    loadFile();
+    //std::cout << "3\n";
+    generateModule();
+    //std::cout << "4\n";
+    runLLVMPasses();
+    // std::cout << "5\n";
+    // generateExecutable();
+    // std::cout << "6\n";
 }
